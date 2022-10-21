15644f07c5bbd3e42c3746a76f2f0f63d796f6db
manyblake <manyblake@gmail.com>
2022-10-21 14:29:49 +0200

    first commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <h1>Elenco di post</h1>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th></th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
+</div>
+@endsection
